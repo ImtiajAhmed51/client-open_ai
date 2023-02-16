929ecce58a8c814d4d4d46c65d44b875cbfa53e9
Imtiaj Anik <imtiajahmedanik@gmail.com>
2023-02-16 23:15:29 +0600

    url change

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ const handleSubmit=async(e)=>{
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response= await fetch('https://long-jade-lion-shoe.cyclic.app/',{
+    const response= await fetch('https://open-ai-server-coyj.onrender.com/',{
       method:'POST',
       headers:{
         'Content-Type':'application/json'
